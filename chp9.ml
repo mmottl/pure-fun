43643328395a529670118685c6c0037cea2749f4
Markus Mottl <markus.mottl@gmail.com>
2012-03-28 15:38:19 -0400

    Improved licensing

--- chp9.ml
+++ chp9.ml
@@ -12,12 +12,17 @@
      email:  markus.mottl@gmail.com
      www:    http://www.ocaml.info
 
-   Unless this violates copyrights of the original sources, the following
-   licence applies to this file:
+   Licensed under the Apache License, Version 2.0 (the "License"); you may
+   not use this file except in compliance with the License.  You may obtain
+   a copy of the License at
 
-   This source code is free software; you can redistribute it and/or
-   modify it without any restrictions. It is distributed in the hope
-   that it will be useful, but WITHOUT ANY WARRANTY.
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+   License for the specific language governing permissions and limitations
+   under the License.
 *)
 
 (************************************************************************)
